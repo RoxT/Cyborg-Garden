95984ad2778b9e50b2a385b32cfddddf992135e6
joeycarlos <jclaguio@gmail.com>
2020-02-01 12:19:23 -0800

    Add announcement on collection

--- Cyborg-Garden/Assets/Scripts/GameManager.cs
+++ Cyborg-Garden/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,6 +17,8 @@ public class GameManager : MonoBehaviour
             return _instance;
         }
     }
+
+    public GameObject announcement;
 
     [HideInInspector] public int joyScore;
     [HideInInspector] public int sadnessScore;
@@ -40,18 +43,31 @@ public class GameManager : MonoBehaviour
         if (ballType == BallType.Joy) {
             joyScore = joyScore + increaseValue;
             GameplayUI.Instance.UpdateScore(ballType, joyScore);
+            DisplayAnnouncement("A feeling of joy washes over you.", Color.yellow);
         } else if (ballType == BallType.Sadness) {
             sadnessScore = sadnessScore + increaseValue;
             GameplayUI.Instance.UpdateScore(ballType, sadnessScore);
+            DisplayAnnouncement("You feel inexplicable sadness...", Color.blue);
         } else if (ballType == BallType.Anger) {
             angerScore = angerScore + increaseValue;
             GameplayUI.Instance.UpdateScore(ballType, angerScore);
+            DisplayAnnouncement("Rage fills your mind", Color.red);
         } else if (ballType == BallType.Fear) {
             fearScore = fearScore + increaseValue;
             GameplayUI.Instance.UpdateScore(ballType, fearScore);
+            DisplayAnnouncement("An intense fear rushes through you", Color.magenta);
         } else if (ballType == BallType.Disgust) {
             disgustScore = disgustScore + increaseValue;
             GameplayUI.Instance.UpdateScore(ballType, disgustScore);
+            DisplayAnnouncement("Suddenly you want to barf...", Color.green);
         }
+    }
+
+    public void DisplayAnnouncement(string s, Color color) {
+        GameObject g = Instantiate(announcement, GameplayUI.Instance.transform);
+        Text t = g.GetComponent<Text>();
+        t.text = s;
+        t.color = color;
+        Destroy(g, 1.0f);
     }
 }
