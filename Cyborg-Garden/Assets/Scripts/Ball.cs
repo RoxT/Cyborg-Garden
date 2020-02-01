07d071c7c5424344ce16d246705ba0ead40ed188
joeycarlos <jclaguio@gmail.com>
2020-02-01 10:48:57 -0800

    Add Joey base scene and prefabs

---
+++ Cyborg-Garden/Assets/Scripts/Ball.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private BallType ballType;
+
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        InitBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitBall() {
+        ballType = (BallType)Random.Range(0, 5);
+        if (ballType == BallType.Joy) {
+            sr.color = Color.yellow;
+        }
+        else if (ballType == BallType.Sadness) {
+            sr.color = Color.blue;
+        }
+        else if (ballType == BallType.Anger) {
+            sr.color = Color.red;
+        }
+        else if (ballType == BallType.Disgust) {
+            sr.color = Color.green;
+        } else if (ballType == BallType.Fear) {
+            sr.color = Color.magenta;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Collector") {
+            Collector c = collision.gameObject.GetComponent<Collector>();
+            if (c.collectorType == ballType) {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
+
+public enum BallType {
+    Joy,
+    Sadness,
+    Anger,
+    Disgust,
+    Fear
+}
---
+++ Cyborg-Garden/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject ball;
+    public float spawnIntervalTime;
+    public float minHorizontalForce;
+    public float maxHorizontalForce;
+
+    private Rigidbody2D ballRb;
+    private float timeSinceSpawn;
+
+    // Start is called before the first frame update
+    void Start() {
+        timeSinceSpawn = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timeSinceSpawn = timeSinceSpawn - Time.deltaTime;
+
+        if (timeSinceSpawn <= 0)
+            SpawnBall();
+    }
+
+    void SpawnBall() {
+        Ball iBall = Instantiate(ball, transform.position, Quaternion.identity).GetComponent<Ball>();
+        Rigidbody2D rb = iBall.GetComponent<Rigidbody2D>();
+        Vector2 initDirection = new Vector2(Random.Range(minHorizontalForce, maxHorizontalForce), 0.0f);
+        rb.AddForce(initDirection);
+        timeSinceSpawn = spawnIntervalTime;
+    }
+}
