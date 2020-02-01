a4b1a7f382b3a6374c29f874827a3f41f367af23
Roxanne Taylor <roxanne.e.taylor@gmail.com>
2020-02-01 10:39:58 -0800

    folder

---
+++ Cyborg-Garden/Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float thrust;
+    new Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        
+        rigidbody.AddForce(transform.up * thrust);
+        //rigidbody.gravityScale = .3f;
+    }
+}
---
+++ Cyborg-Garden/Assets/Scripts/WandController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandController : MonoBehaviour
+{
+
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+    public float strength;
+    public Rigidbody2D ball;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+
+            ball.AddForce((transform.position - ball.transform.position) * strength);
+        }   
+    }
+}
