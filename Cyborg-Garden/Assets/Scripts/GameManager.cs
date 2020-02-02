c6f992a0dd859762479466e1ed6b80bde75115c9
joeycarlos <jclaguio@gmail.com>
2020-02-01 19:15:12 -0800

    Add LED UI

--- Cyborg-Garden/Assets/Scripts/GameManager.cs
+++ Cyborg-Garden/Assets/Scripts/GameManager.cs
@@ -20,6 +20,26 @@ public class GameManager : MonoBehaviour
 
     public GameObject announcement;
 
+    [HideInInspector] public int joyLevel;
+    public int joyLevelThreshold;
+    private int joyPointsThisLevel;
+
+    [HideInInspector] public int sadnessLevel;
+    public int sadnessLevelThreshold;
+    private int sadnessPointsThisLevel;
+
+    [HideInInspector] public int angerLevel;
+    public int angerLevelThreshold;
+    private int angerPointsThisLevel;
+
+    [HideInInspector] public int fearLevel;
+    public int fearLevelThreshold;
+    private int fearPointsThisLevel;
+
+    [HideInInspector] public int disgustLevel;
+    public int disgustLevelThreshold;
+    private int disgustPointsThisLevel;
+
     [HideInInspector] public int joyScore;
     [HideInInspector] public int sadnessScore;
     [HideInInspector] public int angerScore;
@@ -37,29 +57,95 @@ public class GameManager : MonoBehaviour
         angerScore = 0;
         fearScore = 0;
         disgustScore = 0;
+
+        joyPointsThisLevel = 0;
+        sadnessPointsThisLevel = 0;
+        angerPointsThisLevel = 0;
+        fearPointsThisLevel = 0;
+        disgustPointsThisLevel = 0;
+
+        joyLevel = 0;
+        sadnessLevel = 0;
+        angerLevel = 0;
+        fearLevel = 0;
+        disgustLevel = 0;
     }
 
     public void IncreaseScore(BallType ballType, int increaseValue) {
         if (ballType == BallType.Joy) {
             joyScore = joyScore + increaseValue;
+
+            joyPointsThisLevel++;
+            if (joyPointsThisLevel >= joyLevelThreshold) {
+                LevelUpEmotion(ballType);
+                joyPointsThisLevel = 0;
+            }
+
             GameplayUI.Instance.UpdateScore(ballType, joyScore);
             DisplayAnnouncement("A feeling of joy washes over you.", Color.yellow);
         } else if (ballType == BallType.Sadness) {
             sadnessScore = sadnessScore + increaseValue;
+
+            sadnessPointsThisLevel++;
+            if (sadnessPointsThisLevel >= sadnessLevelThreshold) {
+                LevelUpEmotion(ballType);
+                sadnessPointsThisLevel = 0;
+            }
+
             GameplayUI.Instance.UpdateScore(ballType, sadnessScore);
             DisplayAnnouncement("You feel inexplicable sadness...", Color.blue);
         } else if (ballType == BallType.Anger) {
             angerScore = angerScore + increaseValue;
+
+            angerPointsThisLevel++;
+            if (angerPointsThisLevel >= angerLevelThreshold) {
+                LevelUpEmotion(ballType);
+                angerPointsThisLevel = 0;
+            }
+
             GameplayUI.Instance.UpdateScore(ballType, angerScore);
             DisplayAnnouncement("Rage fills your mind", Color.red);
         } else if (ballType == BallType.Fear) {
             fearScore = fearScore + increaseValue;
+
+            fearPointsThisLevel++;
+            if (fearPointsThisLevel >= fearLevelThreshold) {
+                LevelUpEmotion(ballType);
+                fearPointsThisLevel = 0;
+            }
+
             GameplayUI.Instance.UpdateScore(ballType, fearScore);
             DisplayAnnouncement("An intense fear rushes through you", Color.magenta);
         } else if (ballType == BallType.Disgust) {
             disgustScore = disgustScore + increaseValue;
+
+            disgustPointsThisLevel++;
+            if (disgustPointsThisLevel >= disgustLevelThreshold) {
+                LevelUpEmotion(ballType);
+                disgustPointsThisLevel = 0;
+            }
+
             GameplayUI.Instance.UpdateScore(ballType, disgustScore);
             DisplayAnnouncement("Suddenly you want to barf...", Color.green);
+        }
+    }
+
+    public void LevelUpEmotion(BallType ballType) {
+        if (ballType == BallType.Joy) {
+            joyLevel++;
+            GameplayUI.Instance.UpdateLED(ballType, joyLevel - 1);
+        } else if (ballType == BallType.Sadness) {
+            sadnessLevel++;
+            GameplayUI.Instance.UpdateLED(ballType, sadnessLevel - 1);
+        } else if (ballType == BallType.Anger) {
+            angerLevel++;
+            GameplayUI.Instance.UpdateLED(ballType, angerLevel - 1);
+        } else if (ballType == BallType.Fear) {
+            fearLevel++;
+            GameplayUI.Instance.UpdateLED(ballType, fearLevel - 1);
+        } else if (ballType == BallType.Disgust) {
+            disgustLevel++;
+            GameplayUI.Instance.UpdateLED(ballType, disgustLevel - 1);
         }
     }
 
--- Cyborg-Garden/Assets/Scripts/GameplayUI.cs
+++ Cyborg-Garden/Assets/Scripts/GameplayUI.cs
@@ -18,11 +18,28 @@ public class GameplayUI : MonoBehaviour
         }
     }
 
+    public float horizontalSpacing;
+
     public Text joyScore;
     public Text sadnessScore;
     public Text angerScore;
     public Text fearScore;
     public Text disgustScore;
+
+    public GameObject joyLED;
+    public GameObject sadnessLED;
+    public GameObject angerLED;
+    public GameObject fearLED;
+    public GameObject disgustLED;
+
+    private Image[] joyLED_List;
+    private Image[] sadnessLED_List;
+    private Image[] angerLED_List;
+    private Image[] fearLED_List;
+    private Image[] disgustLED_List;
+
+    public Sprite LED_on;
+    public Image LED_off;
 
     void Awake() {
         _instance = this;
@@ -36,6 +53,8 @@ public class GameplayUI : MonoBehaviour
         angerScore.text = "0";
         fearScore.text = "0";
         disgustScore.text = "0";
+
+        GenerateLEDLightUI();
     }
 
     public void UpdateScore(BallType ballType, int newValue) {
@@ -53,6 +72,78 @@ public class GameplayUI : MonoBehaviour
         }
         else if (ballType == BallType.Disgust) {
             disgustScore.text = newValue.ToString();
+        }
+    }
+
+    void GenerateLEDLightUI() {
+        
+        /*
+        Image image = Instantiate(LED_off, GameplayUI.Instance.gameObject.transform);
+        image.gameObject.transform.position = joyLED.transform.position;
+        */
+
+        joyLED_List = new Image[5];
+
+        for (int i = 0; i < 5; i++) {
+            joyLED_List[i] = Instantiate(LED_off, GameplayUI.Instance.gameObject.transform);
+            joyLED_List[i].gameObject.transform.position = joyLED.transform.position + new Vector3 (i * horizontalSpacing, 0, 0);
+        }
+
+        sadnessLED_List = new Image[5];
+
+        for (int i = 0; i < 5; i++) {
+            sadnessLED_List[i] = Instantiate(LED_off, GameplayUI.Instance.gameObject.transform);
+            sadnessLED_List[i].gameObject.transform.position = sadnessLED.transform.position + new Vector3(i * horizontalSpacing, 0, 0);
+        }
+
+        angerLED_List = new Image[5];
+
+        for (int i = 0; i < 5; i++) {
+            angerLED_List[i] = Instantiate(LED_off, GameplayUI.Instance.gameObject.transform);
+            angerLED_List[i].gameObject.transform.position = angerLED.transform.position + new Vector3(i * horizontalSpacing, 0, 0);
+        }
+
+        fearLED_List = new Image[5];
+
+        for (int i = 0; i < 5; i++) {
+            fearLED_List[i] = Instantiate(LED_off, GameplayUI.Instance.gameObject.transform);
+            fearLED_List[i].gameObject.transform.position = fearLED.transform.position + new Vector3(i * horizontalSpacing, 0, 0);
+        }
+
+        disgustLED_List = new Image[5];
+
+        for (int i = 0; i < 5; i++) {
+            disgustLED_List[i] = Instantiate(LED_off, GameplayUI.Instance.gameObject.transform);
+            disgustLED_List[i].gameObject.transform.position = disgustLED.transform.position + new Vector3(i * horizontalSpacing, 0, 0);
+        }
+    }
+
+    public void UpdateLED(BallType ballType, int level) {
+        if (ballType == BallType.Joy) {
+            if (level < 5) {
+                joyLED_List[level].sprite = LED_on;
+                joyLED_List[level].color = Color.yellow;
+            }
+        } else if (ballType == BallType.Sadness) {
+            if (level < 5) {
+                sadnessLED_List[level].sprite = LED_on;
+                sadnessLED_List[level].color = Color.blue;
+            }
+        } else if (ballType == BallType.Anger) {
+            if (level < 5) {
+                angerLED_List[level].sprite = LED_on;
+                angerLED_List[level].color = Color.red;
+            }
+        } else if (ballType == BallType.Fear) {
+            if (level < 5) {
+                fearLED_List[level].sprite = LED_on;
+                fearLED_List[level].color = Color.magenta;
+            }
+        } else if (ballType == BallType.Disgust) {
+            if (level < 5) {
+                disgustLED_List[level].sprite = LED_on;
+                disgustLED_List[level].color = Color.green;
+            }
         }
     }
 }
