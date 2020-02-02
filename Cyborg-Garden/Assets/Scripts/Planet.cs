9391f46dc1915daeb523910db25fab538e835cdc
Roxanne Taylor <roxanne.e.taylor@gmail.com>
2020-02-01 16:02:52 -0800

    Separate planet behaviour

---
+++ Cyborg-Garden/Assets/Scripts/Planet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Rigidbody2D ballRB = collision.GetComponent<Rigidbody2D>();
+        if (ballRB != null && collision.GetComponent<Ball>() != null)
+        {
+            ballRB.AddForce((transform.position - ballRB.transform.position) * force);
+        }
+    }
+}
